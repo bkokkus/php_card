9b0bc42c19c7622025e40c9711a9510e3d7b779c
bkokkus <bekirkokkus@gmail.com>
2019-01-16 02:04:29 +0300

    Front-End bitti

--- index.php
+++ index.php
@@ -13,38 +13,7 @@
 <body>
 
     <!------------- HEADER ------------------------------>
-    <header>
-        <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">KT SHOP</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Ürünler <span class="sr-only">(current)</span></a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="#">
-                        <span class="glyphicon glyphicon-shopping-cart cart-icon"></span>
-                        <span class="badge cart-count">10</span>
-                    </a>
-                </li>
-            
-            </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-        </nav>
-    </header>
+    <?php include "lib/navbar.php"; ?>
 
     <!------------- HEADER ------------------------------>
 
---
+++ shopping-cart.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sepet Sayfası</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="assets/css/custom.css">
+   
+</head>
+<body>
+
+    <?php include "lib/navbar.php"; ?>
+
+    <!---------MAIN CONTENT--------------->
+    <div class="container">
+        <h2 class="text-center"> 
+        Sepetinizde <strong class="color-danger">5</strong> adet ürün bulunmaktadır.
+        </h2>    
+    <hr>
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <table class="table table-hover table-bordered table-striped">
+                <thead>
+                    <th class="text-center">Ürün Resmi</th>
+                    <th class="text-center">Ürün Adı</th>
+                    <th class="text-center">Ürün Fiyatı</th>
+                    <th class="text-center">Adeti</th>
+                    <th class="text-center">Toplam</th>
+                    <th class="text-center">Sepetten Çıkar</th>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td class="text-center">
+                            <img width="50px" src="assets/img/kedi.png" alt="">
+                        </td>
+                        <td class="text-center">bbk Kedi Maması</td>
+                        <td class="text-center"><strong>100 ₺ </strong></td>
+                        <td class="text-center">
+                            <a href="#" class="btn btn-xs btn-success">
+                                <span class="glyphicon glyphicon-plus"></span>
+                            </a>
+                            <input type="text" value="3" class="item-count-input">
+                            <a href="#" class="btn btn-xs btn-danger">
+                                <span class="glyphicon glyphicon-minus"></span>
+                            </a>
+                        </td>
+                        <td class="text-center">300 ₺</td>
+                        <td class="text-center">
+                            <a href="#" class="btn btn-danger btn-sm">
+                            <span class="glyphicon glyphicon-remove"></span> Sepetten Çıkar
+                            </a>
+                        </td>
+                    </tr> 
+
+                    
+                    <tr>
+                        <td class="text-center">
+                            <img width="50px" src="assets/img/kedi.png" alt="">
+                        </td>
+                        <td class="text-center">bbk Kedi Maması</td>
+                        <td class="text-center"><strong>100 ₺ </strong></td>
+                        <td class="text-center">
+                            <a href="#" class="btn btn-xs btn-success">
+                                <span class="glyphicon glyphicon-plus"></span>
+                            </a>
+                            <input type="text" value="3" class="item-count-input">
+                            <a href="#" class="btn btn-xs btn-danger">
+                                <span class="glyphicon glyphicon-minus"></span>
+                            </a>
+                        </td>
+                        <td class="text-center">300 ₺</td>
+                        <td class="text-center">
+                            <a href="#" class="btn btn-danger btn-sm">
+                            <span class="glyphicon glyphicon-remove"></span> Sepetten Çıkar
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">
+                            <img width="50px" src="assets/img/kedi.png" alt="">
+                        </td>
+                        <td class="text-center">bbk Kedi Maması</td>
+                        <td class="text-center"><strong>100 ₺ </strong></td>
+                        <td class="text-center">
+                            <a href="#" class="btn btn-xs btn-success">
+                                <span class="glyphicon glyphicon-plus"></span>
+                            </a>
+                            <input type="text" value="3" class="item-count-input">
+                            <a href="#" class="btn btn-xs btn-danger">
+                                <span class="glyphicon glyphicon-minus"></span>
+                            </a>
+                        </td>
+                        <td class="text-center">300 ₺</td>
+                        <td class="text-center">
+                            <a href="#" class="btn btn-danger btn-sm">
+                            <span class="glyphicon glyphicon-remove"></span> Sepetten Çıkar
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-center">
+                            <img width="50px" src="assets/img/kedi.png" alt="">
+                        </td>
+                        <td class="text-center">bbk Kedi Maması</td>
+                        <td class="text-center"><strong>100 ₺ </strong></td>
+                        <td class="text-center">
+                            <a href="#" class="btn btn-xs btn-success">
+                                <span class="glyphicon glyphicon-plus"></span>
+                            </a>
+                            <input type="text" value="3" class="item-count-input">
+                            <a href="#" class="btn btn-xs btn-danger">
+                                <span class="glyphicon glyphicon-minus"></span>
+                            </a>
+                        </td>
+                        <td class="text-center">300 ₺</td>
+                        <td class="text-center">
+                            <a href="#" class="btn btn-danger btn-sm">
+                            <span class="glyphicon glyphicon-remove"></span> Sepetten Çıkar
+                            </a>
+                        </td>
+                    </tr>
+                  
+                    
+                </tbody>
+
+                <tfoot>
+                    <th colspan="2" class="text-right">
+                        Toplam Ürün : <span class="color-danger">4 adet</span>
+                    </th>            
+                    <th colspan="4" class="text-right">
+                        Toplam Tutar : <span class="color-danger">1200 TL</span>
+                    </th>            
+                </tfoot>
+            </table>
+        </div>
+    </div>
+
+    
+    </div>
+    <!---------MAIN CONTENT--------------->
+
+</body>
+</html>
