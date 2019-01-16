c9ad40cbff1189af7a500eea82f208e157602072
bkokkus <bekirkokkus@gmail.com>
2019-01-16 10:36:28 +0300

    db.php kullanıcı boşaltıldı.

--- lib/db.php
+++ lib/db.php
@@ -1,8 +1,7 @@
 <?php
 
 try {
-    $db = new PDO("mysql:hostname=localhost;dbname=php_cart;charset=utf8","bekir","Test.123");
+    $db = new PDO("mysql:hostname=localhost;dbname=php_cart;charset=utf8","root","");
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-
